1f5cb9a942491db6a4112a88f606faf867510446
Adrián <acasram665l@g.educaand.es>
2025-09-29 12:46:32 +0200

    Ejemplo 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 @Slf4j
@@ -17,13 +20,18 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        File fichero = new File("ejemplo.txt");
-        if (fichero.createNewFile()) {
-            log.info("Fichero creado: {}",  fichero.getName());
-        } else {
-            log.info("El fichero ya existe."); //¿cómo se dónde está?
+        Path ruta = Paths.get("ejemploNIO.txt");
+        if (!Files.exists(ruta)) {
+            Files.createFile(ruta);
+            log.info("Fichero creado con NIO.2");
         }
+        // Escribir texto en el fichero
+        Files.write(ruta, "Hola mundo desde NIO.2".getBytes());
+        // Leer el contenido
+        String contenido = Files.readString(ruta);
+        log.info("Contenido: " + contenido);
     }
 }
+
 
 
